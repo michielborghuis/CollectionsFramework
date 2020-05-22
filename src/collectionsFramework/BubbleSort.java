e21fcd8eb955c752bf3aa31e851073908f94ac86
michielborghuis <59872213+michielborghuis@users.noreply.github.com>
2020-05-22 21:46:39 +0200

    commit

--- src/collectionsFramework/BubbleSort.java
+++ src/collectionsFramework/BubbleSort.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class BubbleSort {
-    private ArrayList<String> str;
+    private ArrayList<String> lst;
 
-    public BubbleSort(ArrayList<String> str) {
-        this.str = str;
+    public BubbleSort(ArrayList<String> lst){
+        this.lst = lst;
     }
 
     public ArrayList<String> bubbleSortChar(){
-        for (int i = 0; i < str.size(); i++){
-            for (int j = i + 1; j < str.size(); j++){
+        for (int i = 0; i < lst.size(); i++){
+            for (int j = i + 1; j < lst.size(); j++){
                 //vergelijk 2 characters die naast elkaar staan
-                if (str.get(j).compareTo(str.get(i)) < 0){
-                    Collections.swap(str, i, j);
+                if (lst.get(j).compareTo(lst.get(i)) < 0){
+                    Collections.swap(lst, i, j);
                 }
             }
         }
-        return str;
+        return lst;
     }
 }
--- src/collectionsFramework/Main.java
+++ src/collectionsFramework/Main.java
@@ -7,20 +7,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-
-        ArrayList<String> words = new ArrayList<>(Arrays.asList("deze", "woorden", "zijn", "zonder", "twijfel", "allemaal", "willekeurig", "gekozen"));
-        BubbleSort s = new BubbleSort(words);
-        System.out.println(s.bubbleSortChar());
-
-
-        RandomLetterCreator r = new RandomLetterCreator(10, "abcdefghijklmnopqrstuvwxyz");
-        String[] letters = r.createArray();
-        System.out.println(Arrays.toString(letters));
-        AlphSort a = new AlphSort(letters);
-        System.out.println(Arrays.toString(a.bubbleSortChar()));
-
-
         Random rand = new Random();
+
+        //Arraylist met meerdere woorden
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("deze", "woorden", "zijn", "zonder", "twijfel", "allemaal", "willekeurig", "gekozen"));
+        BubbleSort w = new BubbleSort(words);
+        System.out.println(w.bubbleSortChar());
+
+
+        //Arraylist met integers
         ArrayList<Integer> getallen = new ArrayList<>();
         for(int i = 0; i < 10; i++){
             int getal = rand.nextInt(10)+1;
@@ -28,5 +23,13 @@ public class Main {
         }
         BubbleSortOne sort = new BubbleSortOne(getallen);
         System.out.println(sort.bubbleSortAll());
+
+
+        //String[] met letters
+        RandomLetterCreator r = new RandomLetterCreator(10, "abcdefghijklmnopqrstuvwxyz");
+        String[] letters = r.createArray();
+        System.out.println(Arrays.toString(letters));
+        AlphSort a = new AlphSort(letters);
+        System.out.println(Arrays.toString(a.bubbleSortChar()));
     }
 }
